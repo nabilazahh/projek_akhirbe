045517924f723fe2989f8b81d5b3e1ce790b364f
nabilazahh <91738296+nabilazahh@users.noreply.github.com>
2021-11-24 13:23:42 +0700

    Add files via upload

---
+++ drakor.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2021 at 07:23 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `datadrakor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drakor`
+--
+
+CREATE TABLE `drakor` (
+  `Title` varchar(100) DEFAULT NULL,
+  `Year` varchar(100) DEFAULT NULL,
+  `Genre` varchar(100) DEFAULT NULL,
+  `Season` varchar(100) DEFAULT NULL,
+  `Episode` varchar(100) DEFAULT NULL,
+  `Main Lead` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
